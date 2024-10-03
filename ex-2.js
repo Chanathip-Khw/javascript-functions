007a37cd561a08f32e83d3d7c7e86cf37a8196a0
Oat Chana <chanathip0944729842@gmail.com>
2024-10-03 13:19:26 +0700

    feat:add answer function assignment

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,20 @@
-// Start coding here
+let add = function(a,b) {
+    return a+b
+}
+
+let subtract = function(a,b) {
+    return a-b
+}
+
+let multiply = function(a,b) {
+    return a*b
+}
+
+let divide = function(a,b) {
+    return a/b
+}
+
+console.log(add(5,2))
+console.log(subtract(5,2))
+console.log(multiply(5,2))
+console.log(divide(5,2))
